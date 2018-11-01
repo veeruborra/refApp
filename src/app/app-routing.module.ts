2acb42a325016ecdbbf7cd838ec182a6cf82c100
veerendra <veeru.borra@gmail.com>
2018-11-01 22:43:08 +0530

    new repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '../../node_modules/@ng-bootstrap/ng-bootstrap';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login' , component: LoginComponent },
+  { path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+},
+{ path: '**', component: LoginComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [NgbModule,RouterModule.forRoot(routes)],
+  declarations: [LoginComponent],
+  exports: [NgbModule,RouterModule,LoginComponent]
 })
 export class AppRoutingModule { }
